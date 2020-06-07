22691d14bbd63df31cc02564be5fbf1b4e888005
ffreemt <yucongo+fmt@gmail.com>
2020-06-07 22:06:46 +0800

    bumped to v0.0.6

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ _ = Path(f'{dir_name}/__init__.py').read_text(encoding='utf-8')
 version, *_ = re.findall(r"__version__\W*=\W*'([^']+)'", _)
 targz = 'v_' + version.replace('.', '') + '.tar.gz'
 
-install_requires = ['httpx', 'tqdm', 'redis', 'cchardet', 'pytest', 'logzero', 'sacremoses']
+install_requires = ['httpx', 'tqdm', 'redis', 'pytest',
+    'logzero', 'sacremoses',
+    'numpy',
+]
 
 README_rst = f'{curr_dir}/README.md'
 long_description = (
